06da8c0fa57928eb76c53079d37065cd2dbfee77
jiaweili-hammer <davidli1031@gmail.com>
2020-12-05 03:09:36 -0500

    partially pick-up objects

--- test/src/main.py
+++ test/src/main.py
@@ -12,6 +12,12 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 import my_demotest
 
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from std_srvs.srv import Empty
+import argparse
+import time
+
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('testmove_kinova', anonymous=True)
 
@@ -23,6 +29,39 @@ hand_group = moveit_commander.MoveGroupCommander("gripper")
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planner_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
 
+
+rospy.wait_for_service('/gazebo/unpause_physics')
+unpause_gazebo = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+resp = unpause_gazebo()
+
+def moveFingers (check,jointcmds,prefix,nbJoints):
+  topic_name = '/' + prefix + '/effort_finger_trajectory_controller/command'
+  pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)  
+  jointCmd = JointTrajectory()  
+  point = JointTrajectoryPoint()
+  jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);  
+  point.time_from_start = rospy.Duration.from_sec(5.0)
+  print("here")
+  for i in range(0, nbJoints):
+    jointCmd.joint_names.append(prefix +'_joint_finger_'+str(i+1))
+    if check == "open":
+    	
+		point.positions.append(0.8)
+		point.velocities.append(0)
+		point.accelerations.append(0)
+		point.effort.append(0)
+    else:
+    	point.positions.append(0.9)
+    	point.velocities.append(-0.05)
+        point.accelerations.append(-0.05)
+        point.effort.append(1.7)
+  jointCmd.points.append(point)
+  rate = rospy.Rate(100)
+  count = 0
+  while (count < 500):
+    pub.publish(jointCmd)
+    count = count + 1
+    rate.sleep()
 #Set and move the initial starting position of the arm
 #Quaternion used here for roation of the end effector
 pose_goal = geometry_msgs.msg.Pose()
@@ -40,15 +79,10 @@ plan = move_group.go(wait=True)
 move_group.stop()
 move_group.clear_pose_targets()
 
-joint_goal = [0, 0, 0]
-joint_goal[0] = pi/4
-joint_goal[1] = pi/4
-joint_goal[2] = pi/4
-plan = hand_group.plan(joint_goal)
-hand_group.go(wait=True)
+moveFingers("open",[0.8,0.8,0.8],'j2n6s300',3)
 
 #INITIALIZING VARIABLES FOR THE LOOP
-i = 0   #COUNTER FOR EACH BLOCK
+i = 6   #COUNTER FOR EACH BLOCK
 callFunction = []
 callFunction = my_demotest.BlockLocate(i) #CAMERA CALL FUNCTION
 tower_x = [-0.2, -0.26, -0.32,-0.2, -0.26, -0.32,-0.2, -0.26, -0.32] #X POSITION FOR TOWER
@@ -56,7 +90,7 @@ tower_y = [0.2, 0.2, 0.2, 0.26, 0.26, 0.26, 0.32, 0.32, 0.32] #Y POSITION FOR TO
 
 block_x = [0.2, 0.2, 0.2, 0.3, 0.3, 0.3, 0.4, 0.4, 0.4]
 block_y = [0.1, 0.2, 0.3,0.1, 0.2, 0.3, 0.1, 0.2, 0.3]
-while callFunction[0] == True:
+while i < 10:
     posx = block_x[i]
     posy = block_y[i]
 
@@ -87,13 +121,10 @@ while callFunction[0] == True:
     plan = move_group.go(wait=True)
     move_group.stop()
     move_group.clear_pose_targets()
-
+	
+	#close the gripper using effort-controller
     print("close gripper")
-    joint_goal[0] = pi/3.35
-    joint_goal[1] = pi/3.35
-    joint_goal[2] = pi/3.35
-    plan = hand_group.plan(joint_goal)
-    hand_group.go(wait=True)
+    moveFingers("close",[-0.5,-0.5,-0.5],'j2n6s300',3)
 
     #RAISE ARM BACK UP IN SAME X-Y POSITION
     pose_goal.orientation.w = 0
@@ -142,11 +173,7 @@ while callFunction[0] == True:
 
     #DROPPING THE BLOCK TO DESIRED LOCATION
     print("release block")
-    joint_goal[0] = pi/4
-    joint_goal[1] = pi/4
-    joint_goal[2] = pi/4
-    plan = hand_group.plan(joint_goal)
-    hand_group.go(wait=True)
+    moveFingers("open",[0.8,0.8,0.8],'j2n6s300',3)
     
     #INSTITUTING NEW VALUES FOR NEXT BLOCK
     
